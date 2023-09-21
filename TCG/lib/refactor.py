596adcc9e6cf3fe727bfb2f9546da9021a6c8a29
Tommy Lu - OA <lut@fortinet.com>
2023-09-21 21:42:06 +0800

    feat(TCG): add CaseRefactor class with methods for updating update schema file and test data generation rule.

--- TCG/lib/refactor.py
+++ TCG/lib/refactor.py
@@ -16,6 +16,23 @@ class CaseRefactor:
         update_query_rule_enum_name(issue: dict, doc: dict) -> None:
             Updates the query rule enum name for a given issue and document (not implemented).
     """
+    
+    @classmethod
+    def update_test_cases(cls, issue_list: list, new_doc: dict) -> None:
+        """
+        Update the existing test cases according to the issue list's trigger action.
+        """
+        for issue in issue_list:
+            if issue['trigger_action'] == None:
+                print(issue['trigger_action'])
+                print(f"No trigger action for this '{issue['severity']}' issue '{issue['field']}'. Please update it manually.")
+            elif issue['trigger_action'] == 'Update Schema':
+                print("Run Update Schema Process...")
+                cls.update_schema_rule(issue, new_doc)
+            elif issue['trigger_action'] == 'Update Query Parameter Name':
+                cls.update_query_rule_name(issue, new_doc)
+            elif issue['trigger_action'] == 'Update Query Parameter Enum Name':
+                cls.update_query_rule_enum_name(issue, new_doc)
 
     @classmethod
     def update_query_rule_name(cls, issue: dict, new_doc: dict) -> None:
@@ -69,18 +86,7 @@ class CaseRefactor:
                 f.truncate()
 
     @classmethod
-    def update_query_rule_enum_name(cls, issue: dict, doc: dict):
-        """
-        Updates the query rule enum name for a given issue and document (not implemented).
-
-        Args:
-            issue (dict): The openapi doc issue that needs to be updated.
-            doc (dict): The openapi doc.
-        """
-        # Currently, Query does not render enums, so no implementation is needed.
-        pass
-    
-    def add_assertion_rule(self, issue: dict, doc: dict) -> None:
+    def add_assertion_rule(cls, issue: dict, doc: dict) -> None:
         """
         Adds a new assertion rule for a given issue and document.
 
@@ -110,8 +116,9 @@ class CaseRefactor:
                 f.truncate()
         except FileNotFoundError:
             logging.error(f"Cannot find '{assertion_path}'. Please update it manually.")
-            
-    def remove_assertion_rule(self, issue: dict, doc: dict) -> None:
+    
+    @classmethod
+    def remove_assertion_rule(cls, issue: dict, doc: dict) -> None:
         """
         Removes an assertion rule for a given issue and document.
 
@@ -136,8 +143,9 @@ class CaseRefactor:
                 f.truncate()
         except FileNotFoundError:
             logging.error(f"Cannot find '{assertion_path}'. Please update it manually.")
-            
-    def update_schema_rule(self, issue: dict, doc: dict) -> None:
+    
+    @classmethod        
+    def update_schema_rule(cls, issue: dict, doc: dict) -> None:
         """
         Updates the schema rule for a given issue and document.
 
@@ -145,34 +153,53 @@ class CaseRefactor:
             issue (dict): The openapi doc issue that needs to be updated.
             doc (dict): The openapi doc.
         """
-    
-        # 做法：先找到issue所在的api，然后找到对应的response，然后找到对应的schema，然后找到对应的field，然后更新。
-        # 先按照影響的 API 來找 request 再找 response 再找 schema 再找 field 再更新
-        # 1. 找到影響的 API
+        from lib.diff import DiffFinder
         for api in issue['affected_api_list']:
-            op_id = GeneralTool.parse_api_name_to_op_id(api_name, doc)
-            # 2. 找到 request (有可能沒有 request，所以要先判斷有沒有 request)
-            try:
-                with open(f"../artifacts/GenerationRule/{op_id}.json", 'r+') as f:
-                    rule = json.loads(f.read())
-                    for k, v in rule.items():
-                        if k == issue['field']:
-                            rule[k] = issue['new_value']
-                    f.seek(0)
-                    f.write(json.dumps(rule, indent=4))
-                    f.truncate()
-            except FileNotFoundError:
-                logging.error(f"This API '{api}' does not have request.")
-            # 3. 找到 response
-            #TODO: 目前沒有支持測試 response 的測試策略，所以先不處理 response 的情況。
-            # 4. 找到 schema
-            schema = GeneralTool.find_schema(response)
-            # 5. 找到 field
-            field = GeneralTool.find_field(schema, issue['field'])
-            # 6. 更新 field
-            field['name'] = issue['new_value']
+            # * Find the schema name.
+            path = DiffFinder.parse_key_path(issue['path'])
+            # * Determine the reference API and reference type. request, response, or parameter (in path or query)
+            action_list = DiffFinder.found_reference_api_and_ref_type(path[2], doc)
+            op_id = GeneralTool.parse_api_name_to_op_id(api, doc)
+            
+            for action in action_list:
+                action_type = action.split(' ')[-1]
+                if action_type == 'Request':
+                    logging.debug("Run Update Schema Request Process...")
+                    cls._update_request_body_with_new_schema(op_id, issue, path)
+                elif action_type == 'Response':
+                    logging.debug("For now, we do not support updating response field. Please update it manually.")
+                    pass
+                elif action_type == 'Path':
+                    pass
+                elif action_type == 'Query':
+                    pass
+                elif action_type == 'Header':
+                    logging.info(f"For now, we do not support updating header field. Please update it manually.")
+                    pass
+                elif action_type == 'Cookie':
+                    logging.info(f"For now, we do not support updating cookie field. Please update it manually.")
+                    pass
 
-        
-        
+    @classmethod
+    def _update_request_body_with_new_schema(cls, op_id: str ,issue: dict, path: list) -> None:
+        """
+        Updates the request body with new schema for a given issue and document.
+
+        Args:
+            issue (dict): The openapi doc issue that needs to be updated.
+            doc (dict): The openapi doc.
+        """       
+        key, field = GeneralTool.parse_field_path_to_key(path)
+        try:
+            with open(f"../artifacts/GenerationRule/{op_id}.json", 'r+') as f:
+                rule = json.loads(f.read())
+                logging.debug(f"rule: {rule[key]}")
+                
+                # f.seek(0)
+                # f.write(json.dumps(rule, indent=4))
+                # f.truncate()
+        except FileNotFoundError:
+            pass
+            logging.error(f"This API '{op_id}' does not have request.")
 
 
