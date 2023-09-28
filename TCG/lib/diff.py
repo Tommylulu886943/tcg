6fa4a69fa5c3b353a23bc27db3a858bfa7b7392d
Tommy Lu - NB <lut@fortinet.com>
2023-09-28 20:20:39 +0800

    feat(TCG): added logic to handle different scenarios when a dictionary entry is removed, added, updated

--- TCG/lib/diff.py
+++ TCG/lib/diff.py
@@ -8,6 +8,7 @@ from deepdiff import DeepDiff
 from collections import OrderedDict
 
 from lib.refactor import CaseRefactor
+from lib.general_tool import GeneralTool
 
 """
 The `DiffFinder` class provides methods for parsing key paths, searching for schemas in a Swagger/OpenAPI document, and finding APIs that use a given schema name.
@@ -46,7 +47,7 @@ class DiffFinder:
         parsed_path = parsed_path.replace("['", "")
         parsed_path = parsed_path.replace("']", "")
         parsed_path = parsed_path.replace("[", ".")
-        parsed_path = parsed_path.replace("]", ".")
+        parsed_path = parsed_path.replace("]", "")
         parsed_path = parsed_path.split('.')
         
         return parsed_path
@@ -234,22 +235,36 @@ class DiffAnalyzer:
                 'old_value': value['old_value'],
                 'new_value': value['new_value'],
                 'trigger_action': None,
-                'affected_api_list': []
+                'affected_api_list': [],
+                'field': path_list[-1],
+                'severity': "UN-DEFINED"
             }
             
+            # Ex : root['paths']['/user']['get']['responses']['200']['description']
             if path_list[0] == 'paths':
-                api_name = path_list[2].upper() + " " + path_list[1]
-                issue['field'] = 'Path: ' + api_name + " " + path_list[-1]
                 if path_list[3] in ['summary', 'description', 'tags']:
                     issue['severity'] = "INFO"
-                elif path_list[3] in ['requestBody', 'responses', 'operationId']:
+                elif path_list[3] in ['requestBody', 'responses']:
                     issue['severity'] = "BREAK"
                     if path_list[3] == 'requestBody':
                         issue['trigger_action'] = "Update Request Body"
-                    elif path_list[3] == 'responses':
-                        issue['trigger_action'] = "Update Response"
-                    elif path_list[3] == 'operationId':
-                        issue['trigger_action'] = "Update Operation ID"
+                    elif path_list[3] == 'responses':      
+                        if path_list[4].startswith('2'):
+                            if path_list[5] in ['description']:
+                                issue['trigger_action'] = None
+                                issue['severity'] = "INFO"
+                            else:
+                                issue['trigger_action'] = "Update Postive Response"   
+                        else:
+                            if path_list[5] in ['description']:
+                                issue['trigger_action'] = None
+                                issue['severity'] = "INFO"
+                            else:
+                                issue['trigger_action'] = "Update Negative Response"   
+           
+                elif path_list[3] == 'operationId':
+                    issue['trigger_action'] = "Update Operation ID"
+                        
                 elif path_list[3] == 'parameters':
                     issue['severity'] = "BREAK"
                     # * Check if parametere is in path or query to determine trigger aciton type.
@@ -269,77 +284,143 @@ class DiffAnalyzer:
                                     issue['trigger_action'] = "Update Query Parameter Type"
                                 elif path_list[6] == 'enum':
                                     issue['trigger_action'] = "Update Query Parameter Enum Name"
-                    else:
-                        issue['field'] = path_list[0].capitalize() + ": " + path_list[-1]
-                        issue['severity'] = "UN-DEFINED"
-                                
-                else:
-                    issue['field'] = path_list[0].capitalize() + ": " + path_list[-1]
-                    issue['severity'] = "UN-DEFINED"
+                        elif param_info['in'] == 'header':
+                            issue['trigger_action'] = None
+                        elif param_info['in'] == 'cookie':
+                            issue['trigger_action'] = None
+
+                    
             elif path_list[0] == 'components' and path_list[1] == 'schemas':
-                issue['field'] = path_list[-1]
                 issue['severity'] = "BREAK"
                 issue['trigger_action'] = "Update Schema"
-
-                prefix = "#/components/schemas/"
-                schema_name = prefix + path_list[2]
+                SCHEMA_PREFIX = "#/components/schemas/"
+                schema_name = SCHEMA_PREFIX + path_list[2]
                 issue['affected_api_list'].extend(DiffFinder.found_consequence_api(schema_name, new_doc))
-            else:
-                issue['field'] = path_list[0].capitalize() + ": " + path_list[-1]
-                issue['severity'] = "UN-DEFINED"
-                
+
             issue_list.append(issue)
         return issue_list
 
     @classmethod
     def _iterable_item_added_handler(self, diff, new_doc, old_doc):
+        issue_list = []
         for key, value in diff['iterable_item_added'].items():
             path_list = DiffFinder.parse_key_path(key)
             issue = {
                 'path': key,
+                'summary': None,
                 'old_value': value['old_value'],
                 'new_value': value['new_value'],
                 'trigger_action': None,
+                'affected_api_list': [],
+                'field': path_list[-1],
+                'severity': "UN-DEFINED"
             }
+            if path_list[0] == 'paths':
+                if path_list[-1] == 'enum':
+                    issue['severity'] = "BREAK"
+                    issue['trigger_action'] = "Add New Enum"
+                    issue['summary'] = f"The enum {issue['new_value']} is added"
+                    
             issue_list.append(issue)
+        return issue_list
 
     @classmethod
     def _iterable_item_removed_handler(self, diff, new_doc, old_doc):
+        issue_list = []
         for key, value in diff['iterable_item_removed'].items():
             path_list = DiffFinder.parse_key_path(key)
             issue = {
                 'path': key,
-                'old_value': value['old_value'],
-                'new_value': value['new_value'],
+                'summary': None,
+                'old_value': value,
+                'new_value': None,
                 'trigger_action': None,
+                'affected_api_list': [],
+                'field': path_list[-1],
+                'severity': "UN-DEFINED"
             }
+            logging.debug(f"Path List: {path_list}")
+            logging.debug(f"Iterable Item Removed: {issue}")
+            if path_list[0] == 'paths':
+                if path_list[3] == 'requestBody':
+                    if path_list[-2] == 'enum':
+                        issue['severity'] = "BREAK"
+                        issue['trigger_action'] = "Remove Request Body Enum Value"
+                        issue['summary'] = f"The enum {issue['old_value']} is removed."
+                elif path_list[3] == 'responses':
+                    if path_list[-2] == 'enum':
+                        issue['severity'] = "BREAK"
+                        issue['trigger_action'] = None
+                        issue['summary'] = f"The enum {issue['old_value']} is removed."
+                                
             issue_list.append(issue)
+        return issue_list
 
     @classmethod
     def _dictionary_item_added_handler(cls, diff, new_doc, old_doc):
         issue_list = []
+        logging.debug(f"Dictionary Item Added: {diff['dictionary_item_added']}")
         for key in diff['dictionary_item_added']:
             path_list = DiffFinder.parse_key_path(key)
             new_value = DiffFinder.search_schema(path_list, new_doc)
+            path = GeneralTool.parse_field_path_to_key(path_list)
+            issue = {
+                'path': key,
+                'summary': None,
+                'old_value': None,
+                'new_value': new_value,
+                'trigger_action': None,
+                'affected_api_list': [],
+                'field': path_list[-1],
+                'severity': "UN-DEFINED"
+            }
+            
             if path_list[0] == 'paths':
                 if path_list[3] == 'requestBody':
-                    issue = {
-                        'path': key,
-                        'field': path_list[-1],
-                        'severity': 'WARNING',
-                        'old_value': None,
-                        'new_value': new_value,
-                        'trigger_action': "Add New Fields to Request Body",
-                    }
+                    if issue['field'] in ['description']:
+                        issue['severity'] = 'INFO'
+                        issue['trigger_action'] = None
+                        issue['summary'] = f"The description of the request body is added."
+                    elif path_list[6] == 'schema':
+                        if issue['field'] == 'enum':
+                            issue['severity'] = 'BREAK'
+                            issue['trigger_action'] = "Apply New Enum to Request Body"
+                            issue['summary'] = f"The new enum {new_value} is added to the '{path_list[4]}' request body."
+                        elif issue['field'] == 'format':
+                            issue['severity'] = 'BREAK'
+                            issue['trigger_action'] = "Apply New Format to Request Body"
+                            issue['summary'] = f"The new format {new_value} is added to the '{path_list[4]}' request body."
+                        elif issue['field'] == 'required':
+                            issue['severity'] = 'BREAK'
+                            issue['trigger_action'] = "Apply New Required Attribute to Request Body"
+                            issue['summary'] = f"The new required attribute {new_value} is added to the '{path_list[4]}' request body."
+                        elif issue['field'] == 'default':
+                            issue['severity'] = 'WARNING'
+                            issue['trigger_action'] = "Apply New Default Value to Request Body"
+                            issue['summary'] = f"The new default value {new_value} is added to the '{path_list[4]}' request body."
+                        else:
+                            issue['severity'] = 'BREAK'
+                            issue['trigger_action'] = f"Add New Field to Request Body"
+                            issue['summary'] = f"The '{path_list[5]}' request body {path} is added."
                 elif path_list[3] == 'responses':
                     issue = {
                         'path': key,
                         'field': path_list[-1],
+                        'summary': None,
                         'severity': 'BREAK',
                         'old_value': None,
                         'new_value': new_value,
                         'trigger_action': "Add New Response",
                     }
+                    if issue['field'][0] in ['2', '4', '5'] or issue['field'] == 'default':
+                        issue['severity'] = 'BREAK'
+                        issue['trigger_action'] = "Add Assertion for Expected Status Code"
+                        issue['summary'] = f"The response status code {issue['field']} is added. If updated, it will add the assertion for expected status code."
+                    elif issue['field'][0] in ['1', '3']:
+                        issue['severity'] = 'INFO'
+                        issue['trigger_action'] = None
+                        issue['summary'] = f"The response status code {issue['field']} is added."
+ 
                 elif path_list[3] == 'parameters':
                     issue = {
                         'path': key,
@@ -386,15 +467,68 @@ class DiffAnalyzer:
         for key in diff['dictionary_item_removed']:
             path_list = DiffFinder.parse_key_path(key)
             old_value = DiffFinder.search_schema(path_list, old_doc)
+            path = GeneralTool.parse_field_path_to_key(path_list) 
+            issue = {
+                'path': key,
+                'field': path_list[-1],
+                'summary': None,
+                'severity': "UN-DEFINED",
+                'old_value': old_value,
+                'new_value': None,
+                'trigger_action': None,
+                'affected_api_list': []
+            }
+            
             if path_list[0] == 'paths':
-                issue = {
-                    'path': key,
-                    'field': path_list[-1],
-                    'severity': 'BREAK',
-                    'old_value': old_value,
-                    'new_value': None,
-                    'trigger_action': "Remove Field",
-                }
+                if path_list[3] == 'responses':
+                    if issue['field'][0] in ['2', '4', '5'] or issue['field'] == 'default':
+                        issue['severity'] = 'BREAK'
+                        issue['trigger_action'] = "Remove Assertion for Expected Status Code"
+                        issue['summary'] = f"The response status code {issue['field']} is removed. If updated, it will remove the assertion for expected status code."
+                    elif issue['field'][0] in ['1', '3']:
+                        issue['severity'] = 'INFO'
+                        issue['trigger_action'] = None
+                        issue['summary'] = f"The response status code {issue['field']} is removed."
+                    elif issue['field'] == 'description':
+                        issue['severity'] = 'INFO'
+                        issue['trigger_action'] = None
+                        issue['summary'] = f"The response status code {path_list[4]} description is removed."
+                    elif path_list[7] == 'schema':
+                        if issue['field'] in ['type', 'format', 'enum']:
+                            issue['severity'] = 'BREAK'
+                            issue['trigger_action'] = None
+                            issue['summary'] = f"The '{path_list[4]}' response {path} is removed."
+                        else:
+                            issue['severity'] = 'BREAK'
+                            issue['trigger_action'] = None
+                            issue['summary'] = f"The '{path_list[4]}' response '{issue['field']}' field is removed."
+
+            elif path_list[0] == 'parameters':
+                issue['severity'] = 'BREAK'
+                issue['trigger_action'] = "Update API Call"
+                issue['summary'] = f"The parameter {issue['field']} is removed from the operation."
+                
+            elif path_list[0] == 'requestBody':
+                if path_list[1] == 'description':
+                    issue['severity'] = 'INFO'
+                    issue['trigger_action'] = None
+                    issue['summary'] = f"The description of the request body is removed."
+                elif path_list[1] == 'content':
+                    media_type = path_list[2]
+                    if path_list[3] == 'schema':
+                        issue['severity'] = 'BREAK'
+                        issue['trigger_action'] = "Update API Call and Validate Payload"
+                        issue['summary'] = f"The schema of the request body for media type {media_type} is modified."
+                    elif path_list[3] == 'examples':
+                        issue['severity'] = 'INFO'
+                        issue['trigger_action'] = None
+                        issue['summary'] = f"The example of the request body for media type {media_type} is removed."
+                elif path_list[1] == 'required':
+                    issue['severity'] = 'BREAK'
+                    issue['trigger_action'] = f"Update required attribute of the request body in {path_list[2]} media type."
+                    issue['summary'] = f"The 'required' attribute of the request body is removed."
+
+
             elif path_list[0] == 'components' and path_list[1] == 'schemas':
                 PREFIX = '#/components/schemas/'
                 schema_name = PREFIX + path_list[2]
